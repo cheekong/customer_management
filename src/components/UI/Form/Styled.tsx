f1da77a68536cbc0c53a5567733dc15018e721c4
cheekong <cheekong.t@gmail.com>
2019-05-06 01:30:16 +1000

    file renamed missed due to local git config

---
+++ src/components/UI/Form/Styled.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const StyledForm = styled.form `
+    width: 100%;
+    min-width:300px;
+    margin-top:20px;
+    margin-bottom: 20px;
+`;
+
+export const StyledFormTitle = styled.div `
+    display: inline-block;
+    width: 100%;
+    margin-top: 10px;
+    margin-bottom: 10px;
+`;
+
+export const StyledFormContent = styled.div `
+    width: 100%;
+`;
